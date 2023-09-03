684182a7bea1363ae9b4ec50536a7a78b89d111e
mohi2code <55515180+mohi2code@users.noreply.github.com>
2023-09-03 20:08:30 +0300

    :rocket: refatored Login.jsx layout component and moved LoginForm component to a new file, LoginForm.jsx. Created a new custom hook useThemeStyle to easily consume antd theme

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,20 +1,14 @@
 /* eslint-disable react/prop-types */
 import {
   Col,
-  Form,
   Row,
-  Typography,
-  theme,
-  Button,
-  Input,
-  Checkbox
+  Typography
 } from 'antd';
-import { LockOutlined, MailOutlined } from '@ant-design/icons';
-import { useMemo, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import useThemeStyle from '../../hooks/useThemeStyle';
 
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { LoginForm } from './LoginForm';
 
 const firebaseConfig = {
   apiKey: "AIzaSyASapdRtPd5Zo5fxAYmK0Z587Zz0wafkgQ",
@@ -37,29 +31,16 @@ const columnStyle = {
 };
 
 function Login() {
-
   const [loading, setLoading] = useState(false);
 
-  const { token } = theme.useToken();
-
-  const themeStyle = useMemo(() => ({
-    backgroundColor: token.colorPrimaryBg,
-    padding: token.padding,
-    color: token.colorPrimaryText,
-    fontSize: token.fontSize,
-  }), [
-    token.colorPrimaryBg,
-    token.colorPrimaryText,
-    token.fontSize,
-    token.padding
-  ]);
+  const { themeStyle } = useThemeStyle();
 
   return (
     <Row style={{ height: '100vh', ...themeStyle }}>
       <Col span={8} offset={8} style={columnStyle}>
         <Header />
 
-        <LoginForm token={token} loading={loading} setLoading={setLoading} />
+        <LoginForm loading={loading} setLoading={setLoading} />
       </Col>
     </Row>
   );
@@ -70,107 +51,6 @@ function Header() {
     <Typography.Title level={1} style={{ margin: '0 auto 3rem auto' }}>
       Login to your account
     </Typography.Title>
-  );
-}
-
-function LoginForm({ token, loading, setLoading }) {
-  async function onFinish({ email, password }) {
-    try {
-      setLoading(true);
-      const res = await signInWithEmailAndPassword(getAuth(), email, password);
-      console.log(res);
-      setLoading(false);
-    } catch (error) {
-      console.log(error.message);
-      setLoading(false);
-    }
-  }
-
-  return (
-    <Form
-      name='login-form'
-      onFinish={onFinish}
-      size='large'
-      disabled={loading}
-    >
-
-      <EmailField token={token} />
-
-      <PasswordField token={token} />
-
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Link to="">Forgot password?</Link>
-      </Form.Item>
-
-      <SubmitButton loading={loading} />
-
-      <Form.Item>
-        Or <Link to='/register'>Register now!</Link>
-      </Form.Item>
-    </Form>
-  );
-}
-
-function EmailField({ token }) {
-  return (
-    <Form.Item
-      name="email"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your email!',
-        },
-      ]}
-    >
-      <Input
-        type='email'
-        prefix={
-          <MailOutlined style={{ color: `${token.colorTextDisabled}` }} />
-        }
-        placeholder="email"
-      />
-    </Form.Item>
-  );
-}
-
-function PasswordField({ token }) {
-  return (
-    <Form.Item
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your password!',
-        },
-      ]}
-    >
-      <Input
-        prefix={
-          <LockOutlined style={{ color: `${token.colorTextDisabled}` }} />
-        }
-        type="password"
-        placeholder="Password"
-      />
-    </Form.Item>
-  );
-}
-
-function SubmitButton({ loading }) {
-  return (
-    <Form.Item>
-      <Button
-        block
-        loading={loading}
-        type="primary"
-        htmlType="submit"
-      >
-        Log in
-      </Button>
-    </Form.Item>
   );
 }
 
---
+++ src/features/auth/LoginForm.jsx
@@ -0,0 +1,106 @@
+/* eslint-disable react/prop-types */
+import {
+  Form, Button,
+  Input,
+  Checkbox
+} from 'antd';
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import useThemeStyle from '../../hooks/useThemeStyle';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+export function LoginForm({ loading, setLoading }) {
+  const { token } = useThemeStyle();
+
+  async function onFinish({ email, password }) {
+    try {
+      setLoading(true);
+      const res = await signInWithEmailAndPassword(getAuth(), email, password);
+      console.log(res);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+      setLoading(false);
+    }
+  }
+
+  return (
+    <Form
+      name='login-form'
+      onFinish={onFinish}
+      size='large'
+      disabled={loading}
+    >
+
+      <EmailField token={token} />
+
+      <PasswordField token={token} />
+
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Link to="">Forgot password?</Link>
+      </Form.Item>
+
+      <SubmitButton loading={loading} />
+
+      <Form.Item>
+        Or <Link to='/register'>Register now!</Link>
+      </Form.Item>
+    </Form>
+  );
+}
+
+function EmailField({ token }) {
+  return (
+    <Form.Item
+      name="email"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your email!',
+        },
+      ]}
+    >
+      <Input
+        type='email'
+        prefix={<MailOutlined style={{ color: `${token.colorTextDisabled}` }} />}
+        placeholder="email" />
+    </Form.Item>
+  );
+}
+
+function PasswordField({ token }) {
+  return (
+    <Form.Item
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input
+        prefix={<LockOutlined style={{ color: `${token.colorTextDisabled}` }} />}
+        type="password"
+        placeholder="Password" />
+    </Form.Item>
+  );
+}
+function SubmitButton({ loading }) {
+  return (
+    <Form.Item>
+      <Button
+        block
+        loading={loading}
+        type="primary"
+        htmlType="submit"
+      >
+        Log in
+      </Button>
+    </Form.Item>
+  );
+}
