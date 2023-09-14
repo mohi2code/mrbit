883f7f6bc147a73f6aa89c99b725847685a8eddc
mohi2code <55515180+mohi2code@users.noreply.github.com>
2023-09-14 13:56:48 +0300

    :sparkles: AuthGuards results

--- src/app/features/auth/components/AdminGuard.jsx
+++ src/app/features/auth/components/AdminGuard.jsx
@@ -1,11 +1,21 @@
+/* eslint-disable react/prop-types */
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectAccount } from '../authSlice';
+import { Result } from 'antd';
 
 function AdminGuard() {
   const account = useSelector(selectAccount);
 
-  return account?.accountType === 'admin' ? <Outlet /> : <h1>Admin Guard, Access Denied you are a {account?.accountType}</h1>;
+  return account?.accountType === 'admin' ? <Outlet /> : <NotAuthorizedResult subtitle={`Admin Guard, Access Denied you are a ${account?.accountType}`} />;
 }
 
 export default AdminGuard;
+
+export function NotAuthorizedResult({ subtitle }) {
+  return <Result
+    status="403"
+    title="403"
+    subTitle={subtitle}
+  />;
+}
--- src/app/features/auth/components/AdminOrClientGurad.jsx
+++ src/app/features/auth/components/AdminOrClientGurad.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectAccount } from '../authSlice';
+import { NotAuthorizedResult } from './AdminGuard';
 
 function AdminOrClientGuard() {
   const account = useSelector(selectAccount);
 
-  return account?.accountType === 'admin' || account?.accountType === 'client' ? <Outlet /> : <h1>AdminRrClientGuard, Access Denied you are a {account?.accountType}</h1>;
+  return account?.accountType === 'admin' || account?.accountType === 'client' ? <Outlet /> : <NotAuthorizedResult subtitle={`AdminRrClientGuard, Access Denied you are a ${account?.accountType}`} />;
 }
 
 export default AdminOrClientGuard;
